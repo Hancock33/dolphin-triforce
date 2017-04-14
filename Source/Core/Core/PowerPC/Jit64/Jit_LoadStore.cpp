Hancock33/dolphin-triforce

commit e86e1bd52d2e75d0ee4b7fea6ce4e8f4e457b630

Jit_LoadStore: Make dcbz PIE-compliant

Source/Core/Core/PowerPC/Jit64/Jit_LoadStore.cpp
@@ -345,10 +345,11 @@ void Jit64::dcbz(UGeckoInstruction inst)
   if (UReg_MSR(MSR).DR)
   {
     // Perform lookup to see if we can use fast path.
-    MOV(32, R(RSCRATCH2), R(RSCRATCH));
-    SHR(32, R(RSCRATCH2), Imm8(PowerPC::BAT_INDEX_SHIFT));
-    TEST(32, MScaled(RSCRATCH2, SCALE_4, PtrOffset(&PowerPC::dbat_table[0])),
-         Imm32(PowerPC::BAT_PHYSICAL_BIT));
+    MOV(64, R(RSCRATCH2), ImmPtr(&PowerPC::dbat_table[0]));
+    PUSH(RSCRATCH);
+    SHR(32, R(RSCRATCH), Imm8(PowerPC::BAT_INDEX_SHIFT));
+    TEST(32, MComplex(RSCRATCH, RSCRATCH2, SCALE_4, 0), Imm32(PowerPC::BAT_PHYSICAL_BIT));
+    POP(RSCRATCH);
     FixupBranch slow = J_CC(CC_Z, true);
 
     // Fast path: compute full address, then zero out 32 bytes of memory.

commit 69141a77be5498fb3dd52bdad707ae625c1f89a6

Jit_LoadStore: Fix dbat memory access
Was broken by e86e1bd52d2e75d0ee4b7fea6ce4e8f4e457b630

Source/Core/Core/PowerPC/Jit64/Jit_LoadStore.cpp
@@ -348,7 +348,7 @@ void Jit64::dcbz(UGeckoInstruction inst)
     MOV(64, R(RSCRATCH2), ImmPtr(&PowerPC::dbat_table[0]));
     PUSH(RSCRATCH);
     SHR(32, R(RSCRATCH), Imm8(PowerPC::BAT_INDEX_SHIFT));
-    TEST(32, MComplex(RSCRATCH, RSCRATCH2, SCALE_4, 0), Imm32(PowerPC::BAT_PHYSICAL_BIT));
+    TEST(32, MComplex(RSCRATCH2, RSCRATCH, SCALE_4, 0), Imm32(PowerPC::BAT_PHYSICAL_BIT));
     POP(RSCRATCH);
     FixupBranch slow = J_CC(CC_Z, true);
 