dolphin-emu/dolphin

Filesystem: Use file info in arguments instead of path
Some callers already have the file info, making the relatively slow
FindFileInfo calls unnecessary. Callers that didn't have the file info
will now need to call FindFileInfo on their own.

Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -44,31 +44,35 @@ const std::vector<CFileInfoGCWii>& CFileSystemGCWii::GetFileList()
 	return m_FileInfoVector;
 }
 
-const IFileInfo* CFileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
+const IFileInfo* CFileSystemGCWii::FindFileInfo(const std::string& path)
 {
 	if (!m_Initialized)
 		InitFileSystem();
 
 	for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
 	{
-		if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
+		if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), path.c_str()))
 			return &m_FileInfoVector[i];
 	}
 
 	return nullptr;
 }
 
-u64 CFileSystemGCWii::GetFileSize(const std::string& _rFullPath)
+const IFileInfo* CFileSystemGCWii::FindFileInfo(u64 disc_offset)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
+  for (auto& file_info : m_FileInfoVector)
+  {
+    if ((file_info.GetOffset() <= disc_offset) &&
+        ((file_info.GetOffset() + file_info.GetSize()) > disc_offset))
+    {
+      return &file_info;
+    }
+  }
 
-  if (pFileInfo != nullptr && !pFileInfo->IsDirectory())
-    return pFileInfo->GetSize();
-
-  return 0;
+  return nullptr;
 }
 
 std::string CFileSystemGCWii::GetPath(u64 _Address)
@@ -129,43 +133,39 @@ std::string CFileSystemGCWii::GetPathFromFSTOffset(size_t file_info_offset)
   }
 }
 
-u64 CFileSystemGCWii::ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+u64 CFileSystemGCWii::ReadFile(const IFileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                                u64 _OffsetInFile)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
-  if (pFileInfo == nullptr)
+  if (!file_info || file_info->IsDirectory())
     return 0;
 
-  if (_OffsetInFile >= pFileInfo->GetSize())
+  if (_OffsetInFile >= file_info->GetSize())
     return 0;
 
-  u64 read_length = std::min(_MaxBufferSize, pFileInfo->GetSize() - _OffsetInFile);
+  u64 read_length = std::min(_MaxBufferSize, file_info->GetSize() - _OffsetInFile);
 
   DEBUG_LOG(DISCIO, "Reading %" PRIx64 " bytes at %" PRIx64 " from file %s. Offset: %" PRIx64
                     " Size: %" PRIx64,
-            read_length, _OffsetInFile, _rFullPath.c_str(), pFileInfo->GetOffset(),
-            pFileInfo->GetSize());
+            read_length, _OffsetInFile, GetPath(file_info->GetOffset()).c_str(),
+            file_info->GetOffset(), file_info->GetSize());
 
-  m_rVolume->Read(pFileInfo->GetOffset() + _OffsetInFile, read_length, _pBuffer, m_Wii);
+  m_rVolume->Read(file_info->GetOffset() + _OffsetInFile, read_length, _pBuffer, m_Wii);
   return read_length;
 }
 
-bool CFileSystemGCWii::ExportFile(const std::string& _rFullPath,
-                                  const std::string& _rExportFilename)
+bool CFileSystemGCWii::ExportFile(const IFileInfo* file_info, const std::string& _rExportFilename)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
-
-  if (!pFileInfo)
+  if (!file_info || file_info->IsDirectory())
     return false;
 
-  u64 remainingSize = pFileInfo->GetSize();
-  u64 fileOffset = pFileInfo->GetOffset();
+  u64 remainingSize = file_info->GetSize();
+  u64 fileOffset = file_info->GetOffset();
 
   File::IOFile f(_rExportFilename, "wb");
   if (!f)

Source/Core/DiscIO/FileSystemGCWii.h
@@ -41,12 +41,12 @@ public:
   bool IsValid() const override { return m_Valid; }
   const std::vector<CFileInfoGCWii>& GetFileList() override;
   const IFileInfo* FindFileInfo(const std::string& path) override;
-  u64 GetFileSize(const std::string& _rFullPath) override;
+  const IFileInfo* FindFileInfo(u64 disc_offset) override;
   std::string GetPath(u64 _Address) override;
   std::string GetPathFromFSTOffset(size_t file_info_offset) override;
-  u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+  u64 ReadFile(const IFileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                u64 _OffsetInFile) override;
-  bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) override;
+  bool ExportFile(const IFileInfo* file_info, const std::string& _rExportFilename) override;
   bool ExportApploader(const std::string& _rExportFolder) const override;
   bool ExportDOL(const std::string& _rExportFolder) const override;
   u64 GetBootDOLOffset() const override;

Source/Core/DiscIO/Filesystem.h
@@ -38,11 +38,11 @@ public:
   virtual bool IsValid() const = 0;
   // TODO: Should only return IFileInfo, not CFileInfoGCWii
   virtual const std::vector<CFileInfoGCWii>& GetFileList() = 0;
-  virtual u64 GetFileSize(const std::string& _rFullPath) = 0;
   virtual const IFileInfo* FindFileInfo(const std::string& path) = 0;
-  virtual u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+  virtual const IFileInfo* FindFileInfo(u64 disc_offset) = 0;
+  virtual u64 ReadFile(const IFileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) = 0;
-  virtual bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) = 0;
+  virtual bool ExportFile(const IFileInfo* file_info, const std::string& _rExportFilename) = 0;
   virtual bool ExportApploader(const std::string& _rExportFolder) const = 0;
   virtual bool ExportDOL(const std::string& _rExportFolder) const = 0;
   virtual std::string GetPath(u64 _Address) = 0;

Hancock33/dolphin-triforce

Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -50,31 +50,35 @@ const std::vector<FileInfoGCWii>& FileSystemGCWii::GetFileList()
   return m_FileInfoVector;
 }
 
-const FileInfo* FileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
+const FileInfo* FileSystemGCWii::FindFileInfo(const std::string& path)
 {
   if (!m_Initialized)
     InitFileSystem();
 
   for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
   {
-    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
+    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), path.c_str()))
       return &m_FileInfoVector[i];
   }
 
   return nullptr;
 }
 
-u64 FileSystemGCWii::GetFileSize(const std::string& _rFullPath)
+const FileInfo* FileSystemGCWii::FindFileInfo(u64 disc_offset)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
+  for (auto& file_info : m_FileInfoVector)
+  {
+    if ((file_info.GetOffset() <= disc_offset) &&
+        ((file_info.GetOffset() + file_info.GetSize()) > disc_offset))
+    {
+      return &file_info;
+    }
+  }
 
-  if (pFileInfo != nullptr && !pFileInfo->IsDirectory())
-    return pFileInfo->GetSize();
-
-  return 0;
+  return nullptr;
 }
 
 std::string FileSystemGCWii::GetPath(u64 _Address)
@@ -135,42 +139,39 @@ std::string FileSystemGCWii::GetPathFromFSTOffset(size_t file_info_offset)
   }
 }
 
-u64 FileSystemGCWii::ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+u64 FileSystemGCWii::ReadFile(const FileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                               u64 _OffsetInFile)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
-  if (pFileInfo == nullptr)
+  if (!file_info || file_info->IsDirectory())
     return 0;
 
-  if (_OffsetInFile >= pFileInfo->GetSize())
+  if (_OffsetInFile >= file_info->GetSize())
     return 0;
 
-  u64 read_length = std::min(_MaxBufferSize, pFileInfo->GetSize() - _OffsetInFile);
+  u64 read_length = std::min(_MaxBufferSize, file_info->GetSize() - _OffsetInFile);
 
   DEBUG_LOG(DISCIO, "Reading %" PRIx64 " bytes at %" PRIx64 " from file %s. Offset: %" PRIx64
                     " Size: %" PRIx64,
-            read_length, _OffsetInFile, _rFullPath.c_str(), pFileInfo->GetOffset(),
-            pFileInfo->GetSize());
+            read_length, _OffsetInFile, GetPath(file_info->GetOffset()).c_str(),
+            file_info->GetOffset(), file_info->GetSize());
 
-  m_rVolume->Read(pFileInfo->GetOffset() + _OffsetInFile, read_length, _pBuffer, m_partition);
+  m_rVolume->Read(file_info->GetOffset() + _OffsetInFile, read_length, _pBuffer, m_partition);
   return read_length;
 }
 
-bool FileSystemGCWii::ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename)
+bool FileSystemGCWii::ExportFile(const FileInfo* file_info, const std::string& _rExportFilename)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
-
-  if (!pFileInfo)
+  if (!file_info || file_info->IsDirectory())
     return false;
 
-  u64 remainingSize = pFileInfo->GetSize();
-  u64 fileOffset = pFileInfo->GetOffset();
+  u64 remainingSize = file_info->GetSize();
+  u64 fileOffset = file_info->GetOffset();
 
   File::IOFile f(_rExportFilename, "wb");
   if (!f)

Source/Core/DiscIO/FileSystemGCWii.h
@@ -44,12 +44,12 @@ public:
   bool IsValid() const override { return m_Valid; }
   const std::vector<FileInfoGCWii>& GetFileList() override;
   const FileInfo* FindFileInfo(const std::string& path) override;
-  u64 GetFileSize(const std::string& _rFullPath) override;
+  const FileInfo* FindFileInfo(u64 disc_offset) override;
   std::string GetPath(u64 _Address) override;
   std::string GetPathFromFSTOffset(size_t file_info_offset) override;
-  u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+  u64 ReadFile(const FileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                u64 _OffsetInFile) override;
-  bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) override;
+  bool ExportFile(const FileInfo* file_info, const std::string& _rExportFilename) override;
   bool ExportApploader(const std::string& _rExportFolder) const override;
   bool ExportDOL(const std::string& _rExportFolder) const override;
   std::optional<u64> GetBootDOLOffset() const override;

Source/Core/DiscIO/Filesystem.h
@@ -40,11 +40,11 @@ public:
   virtual bool IsValid() const = 0;
   // TODO: Should only return FileInfo, not FileInfoGCWii
   virtual const std::vector<FileInfoGCWii>& GetFileList() = 0;
-  virtual u64 GetFileSize(const std::string& _rFullPath) = 0;
   virtual const FileInfo* FindFileInfo(const std::string& path) = 0;
-  virtual u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
+  virtual const FileInfo* FindFileInfo(u64 disc_offset) = 0;
+  virtual u64 ReadFile(const FileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) = 0;
-  virtual bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) = 0;
+  virtual bool ExportFile(const FileInfo* file_info, const std::string& _rExportFilename) = 0;
   virtual bool ExportApploader(const std::string& _rExportFolder) const = 0;
   virtual bool ExportDOL(const std::string& _rExportFolder) const = 0;
   virtual std::string GetPath(u64 _Address) = 0;