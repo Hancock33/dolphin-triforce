Hancock33/dolphin-triforce

commit 7f1f6a3066d95f75045d5962ed408b315cc0230e

Filesystem: Rewrite finding file info by path for performance
Instead of calling GetPathFromFSTOffset for every file info, FindFileInfo
now only looks at names in directories that are included in the path.
For the common case of searching for "opening.bnr", this means that
only root-level files and directories have to be searched through.

Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -55,10 +55,65 @@ const FileInfo* FileSystemGCWii::FindFileInfo(const std::string& path)
   if (!m_Initialized)
     InitFileSystem();
 
-  for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
+  return FindFileInfo(path, 0);
+}
+
+const FileInfo* FileSystemGCWii::FindFileInfo(const std::string& path,
+                                              size_t search_start_offset) const
+{
+  // Given a path like "directory1/directory2/fileA.bin", this function will
+  // find directory1 and then call itself to search for "directory2/fileA.bin".
+
+  if (path.empty() || path == "/")
+    return &m_FileInfoVector[search_start_offset];
+
+  // It's only possible to search in directories. Searching in a file is an error
+  if (!m_FileInfoVector[search_start_offset].IsDirectory())
+    return nullptr;
+
+  size_t first_dir_separator = path.find('/');
+  const std::string searching_for = path.substr(0, first_dir_separator);
+  const std::string rest_of_path =
+      (first_dir_separator != std::string::npos) ? path.substr(first_dir_separator + 1) : "";
+
+  size_t search_end_offset = m_FileInfoVector[search_start_offset].GetSize();
+  search_start_offset++;
+  while (search_start_offset < search_end_offset)
   {
-    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), path.c_str()))
-      return &m_FileInfoVector[i];
+    const FileInfoGCWii& file_info = m_FileInfoVector[search_start_offset];
+
+    if (file_info.GetName() == searching_for)
+    {
+      // A match is found. The rest of the path is passed on to finish the search.
+      const FileInfo* result = FindFileInfo(rest_of_path, search_start_offset);
+
+      // If the search wasn't successful, the loop continues, just in case there's a second
+      // file info that matches searching_for (which probably won't happen in practice)
+      if (result)
+        return result;
+    }
+
+    if (file_info.IsDirectory())
+    {
+      // Skip a directory and everything that it contains
+
+      if (file_info.GetSize() <= search_start_offset)
+      {
+        // The next offset (obtained by GetSize) is supposed to be larger than
+        // the current offset. If an FST is malformed and breaks that rule,
+        // there's a risk that next offset pointers form a loop.
+        // To avoid infinite loops, this method returns.
+        ERROR_LOG(DISCIO, "Invalid next offset in file system");
+        return nullptr;
+      }
+
+      search_start_offset = file_info.GetSize();
+    }
+    else
+    {
+      // Skip a single file
+      search_start_offset++;
+    }
   }
 
   return nullptr;

Source/Core/DiscIO/FileSystemGCWii.h
@@ -61,6 +61,7 @@ private:
   u32 m_offset_shift;
   std::vector<FileInfoGCWii> m_FileInfoVector;
 
+  const FileInfo* FindFileInfo(const std::string& path, size_t search_start_offset) const;
   std::string GetStringFromOffset(u64 _Offset) const;
   bool DetectFileSystem();
   void InitFileSystem();

commit 6e0059abece4f8380c95c2a4a441d6d03348d8c2

FileSystemGCWii: Use case insensitive comparison in FindFileInfo
This was a regression in 7f1f6a3. Some games seem to name the
banner file OPENING.BNR instead of opening.bnr.

Should fix https://bugs.dolphin-emu.org/issues/10354

Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -285,7 +285,7 @@ std::unique_ptr<FileInfo> FileSystemGCWii::FindFileInfo(const std::string& path,
 
   for (const FileInfo& child : file_info)
   {
-    if (child.GetName() == searching_for)
+    if (!strcasecmp(child.GetName().c_str(), searching_for.c_str()))
     {
       // A match is found. The rest of the path is passed on to finish the search.
       std::unique_ptr<FileInfo> result = FindFileInfo(rest_of_path, child);

Source/Core/DolphinWX/GameListCtrl.h
@@ -125,7 +125,7 @@ private:
   } m_image_indexes;
 
   // Actual backing GameListItems are maintained in a background thread and cached to file
-  static constexpr u32 CACHE_REVISION = 0;
+  static constexpr u32 CACHE_REVISION = 1;  // Last changed in PR 5680
   std::list<std::shared_ptr<GameListItem>> m_cached_files;
   std::thread m_scan_thread;
   Common::Event m_scan_trigger;