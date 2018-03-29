Hancock33/dolphin-triforce

commit 2e9146415a49f5187a86d23bf72a3f95b364eadc

Unify ISOFile (wx) with GameFile (Qt) and put it in UICommon
The original reason I wanted to do this was so that we can replace
the Android-specific code with this in the future, but of course,
just deduplicating between DolphinWX and DolphinQt2 is nice too.

Fixes:

- DolphinQt2 showing the wrong size for split WBFS disc images.

- DolphinQt2 being case sensitive when checking if a file is a DOL/ELF.

- DolphinQt2 not detecting when a Wii banner has become available
after the game list cache was created.

Removes:

- DolphinWX's ability to load PNGs as custom banners. But it was
already rather broken (see https://bugs.dolphin-emu.org/issues/10365
and https://bugs.dolphin-emu.org/issues/10366). The reason I removed
this was because PNG decoding relied on wx code and we don't have any
good non-wx/Qt code for loading PNG files right now (let's not use
SOIL), but we should be able to use libpng directly to implement PNG
loading in the future.

- DolphinQt2's ability to ignore a cached game if the last modified
time differs. We currently don't have a non-wx/Qt way to get the time.

Source/Core/DolphinQt2/GameList/GameTracker.cpp
@@ -7,7 +7,6 @@
 #include <QFile>
 
 #include "DiscIO/DirectoryBlob.h"
-#include "DolphinQt2/GameList/GameFileCache.h"
 #include "DolphinQt2/GameList/GameTracker.h"
 #include "DolphinQt2/QtUtils/QueueOnObject.h"
 #include "DolphinQt2/Settings.h"
@@ -19,13 +18,15 @@ static const QStringList game_filters{
 
 GameTracker::GameTracker(QObject* parent) : QFileSystemWatcher(parent)
 {
-  qRegisterMetaType<QSharedPointer<GameFile>>();
+  qRegisterMetaType<std::shared_ptr<const UICommon::GameFile>>();
   connect(this, &QFileSystemWatcher::directoryChanged, this, &GameTracker::UpdateDirectory);
   connect(this, &QFileSystemWatcher::fileChanged, this, &GameTracker::UpdateFile);
 
-  cache.Load();
+  m_cache.Load();
 
   m_load_thread.Reset([this](const QString& path) { LoadGame(path); });
+
+  // TODO: When language changes, reload m_title_database and call m_cache.UpdateAdditionalMetadata
 }
 
 void GameTracker::AddDirectory(const QString& dir)
@@ -130,25 +131,12 @@ void GameTracker::UpdateFile(const QString& file)
 
 void GameTracker::LoadGame(const QString& path)
 {
-  if (!DiscIO::ShouldHideFromGameList(path.toStdString()))
+  const std::string converted_path = path.toStdString();
+  if (!DiscIO::ShouldHideFromGameList(converted_path))
   {
-    if (cache.IsCached(path))
-    {
-      const QDateTime last_modified = QFileInfo(path).lastModified();
-      auto cached_file = cache.GetFile(path);
-      if (cached_file.GetLastModified() >= last_modified)
-      {
-        emit GameLoaded(QSharedPointer<GameFile>::create(cached_file));
-        return;
-      }
-    }
-
-    auto game = QSharedPointer<GameFile>::create(path);
-    if (game->IsValid())
-    {
-      emit GameLoaded(game);
-      cache.Update(*game);
-      cache.Save();
-    }
+    bool cache_changed = false;
+    emit GameLoaded(m_cache.AddOrGet(converted_path, &cache_changed, m_title_database));
+    if (cache_changed)
+      m_cache.Save();
   }
 }

Source/Core/UICommon/GameFileCache.cpp
@@ -1,0 +1,235 @@
+// Copyright 2017 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include "UICommon/GameFileCache.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include "Common/ChunkFile.h"
+#include "Common/CommonTypes.h"
+#include "Common/File.h"
+#include "Common/FileSearch.h"
+#include "Common/FileUtil.h"
+
+#include "Core/TitleDatabase.h"
+
+#include "DiscIO/DirectoryBlob.h"
+
+#include "UICommon/GameFile.h"
+
+namespace UICommon
+{
+static constexpr u32 CACHE_REVISION = 7;  // Last changed in PR 6281
+
+std::vector<std::string> FindAllGamePaths(const std::vector<std::string>& directories_to_scan,
+                                          bool recursive_scan)
+{
+  static const std::vector<std::string> search_extensions = {
+      ".gcm", ".tgc", ".iso", ".ciso", ".gcz", ".wbfs", ".wad", ".dol", ".elf"};
+
+  // TODO: We could process paths iteratively as they are found
+  return Common::DoFileSearch(directories_to_scan, search_extensions, recursive_scan);
+}
+
+void GameFileCache::ForEach(std::function<void(const std::shared_ptr<const GameFile>&)> f) const
+{
+  for (const std::shared_ptr<const GameFile>& item : m_cached_files)
+    f(item);
+}
+
+void GameFileCache::Clear()
+{
+  m_cached_files.clear();
+}
+
+std::shared_ptr<const GameFile> GameFileCache::AddOrGet(const std::string& path,
+                                                        bool* cache_changed,
+                                                        const Core::TitleDatabase& title_database)
+{
+  auto it = std::find_if(
+      m_cached_files.begin(), m_cached_files.end(),
+      [&path](const std::shared_ptr<GameFile>& file) { return file->GetFilePath() == path; });
+  const bool found = it != m_cached_files.cend();
+  if (!found)
+    m_cached_files.emplace_back(std::make_shared<GameFile>(path));
+  std::shared_ptr<GameFile>& result = found ? *it : m_cached_files.back();
+  if (UpdateAdditionalMetadata(&result, title_database) || !found)
+    *cache_changed = true;
+
+  return result;
+}
+
+bool GameFileCache::Update(const std::vector<std::string>& all_game_paths)
+{
+  // Copy game paths into a set, except ones that match DiscIO::ShouldHideFromGameList.
+  // TODO: Prevent DoFileSearch from looking inside /files/ directories of DirectoryBlobs at all?
+  // TODO: Make DoFileSearch support filter predicates so we don't have remove things afterwards?
+  std::unordered_set<std::string> game_paths;
+  game_paths.reserve(all_game_paths.size());
+  for (const std::string& path : all_game_paths)
+  {
+    if (!DiscIO::ShouldHideFromGameList(path))
+      game_paths.insert(path);
+  }
+
+  bool cache_changed = false;
+
+  // Delete paths that aren't in game_paths from m_cached_files,
+  // while simultaneously deleting paths that aren't in m_cached_files from game_paths.
+  // For the sake of speed, we don't care about maintaining the order of m_cached_files.
+  {
+    auto it = m_cached_files.begin();
+    auto end = m_cached_files.end();
+    while (it != end)
+    {
+      if (game_paths.erase((*it)->GetFilePath()))
+      {
+        ++it;
+      }
+      else
+      {
+        cache_changed = true;
+        --end;
+        *it = std::move(*end);
+      }
+    }
+    m_cached_files.erase(it, m_cached_files.end());
+  }
+
+  // Now that the previous loop has run, game_paths only contains paths that
+  // aren't in m_cached_files, so we simply add all of them to m_cached_files.
+  for (const auto& path : game_paths)
+  {
+    auto file = std::make_shared<GameFile>(path);
+    if (file->IsValid())
+    {
+      cache_changed = true;
+      m_cached_files.push_back(std::move(file));
+    }
+  }
+
+  return cache_changed;
+}
+
+bool GameFileCache::UpdateAdditionalMetadata(const Core::TitleDatabase& title_database)
+{
+  bool cache_changed = false;
+
+  for (auto& file : m_cached_files)
+    cache_changed |= UpdateAdditionalMetadata(&file, title_database);
+
+  return cache_changed;
+}
+
+bool GameFileCache::UpdateAdditionalMetadata(std::shared_ptr<GameFile>* game_file,
+                                             const Core::TitleDatabase& title_database)
+{
+  const bool emu_state_changed = (*game_file)->EmuStateChanged();
+  const bool banner_changed = (*game_file)->BannerChanged();
+  const bool custom_title_changed = (*game_file)->CustomNameChanged(title_database);
+  if (!emu_state_changed && !banner_changed && !custom_title_changed)
+    return false;
+
+  // If a cached file needs an update, apply the updates to a copy and delete the original.
+  // This makes the usage of cached files in other threads safe.
+
+  std::shared_ptr<GameFile> copy = std::make_shared<GameFile>(**game_file);
+  if (emu_state_changed)
+    copy->EmuStateCommit();
+  if (banner_changed)
+    copy->BannerCommit();
+  if (custom_title_changed)
+    copy->CustomNameCommit();
+  *game_file = std::move(copy);
+
+  return true;
+}
+
+bool GameFileCache::Load()
+{
+  return SyncCacheFile(false);
+}
+
+bool GameFileCache::Save()
+{
+  return SyncCacheFile(true);
+}
+
+bool GameFileCache::SyncCacheFile(bool save)
+{
+  std::string filename(File::GetUserPath(D_CACHE_IDX) + "gamelist.cache");
+  const char* open_mode = save ? "wb" : "rb";
+  File::IOFile f(filename, open_mode);
+  if (!f)
+    return false;
+  bool success = false;
+  if (save)
+  {
+    // Measure the size of the buffer.
+    u8* ptr = nullptr;
+    PointerWrap p(&ptr, PointerWrap::MODE_MEASURE);
+    DoState(&p);
+    const size_t buffer_size = reinterpret_cast<size_t>(ptr);
+
+    // Then actually do the write.
+    std::vector<u8> buffer(buffer_size);
+    ptr = &buffer[0];
+    p.SetMode(PointerWrap::MODE_WRITE);
+    DoState(&p, buffer_size);
+    if (f.WriteBytes(buffer.data(), buffer.size()))
+      success = true;
+  }
+  else
+  {
+    std::vector<u8> buffer(f.GetSize());
+    if (buffer.size() && f.ReadBytes(buffer.data(), buffer.size()))
+    {
+      u8* ptr = buffer.data();
+      PointerWrap p(&ptr, PointerWrap::MODE_READ);
+      DoState(&p, buffer.size());
+      if (p.GetMode() == PointerWrap::MODE_READ)
+        success = true;
+    }
+  }
+  if (!success)
+  {
+    // If some file operation failed, try to delete the probably-corrupted cache
+    f.Close();
+    File::Delete(filename);
+  }
+  return success;
+}
+
+void GameFileCache::DoState(PointerWrap* p, u64 size)
+{
+  struct
+  {
+    u32 revision;
+    u64 expected_size;
+  } header = {CACHE_REVISION, size};
+  p->Do(header);
+  if (p->GetMode() == PointerWrap::MODE_READ)
+  {
+    if (header.revision != CACHE_REVISION || header.expected_size != size)
+    {
+      p->SetMode(PointerWrap::MODE_MEASURE);
+      return;
+    }
+  }
+  p->DoEachElement(m_cached_files, [](PointerWrap& state, std::shared_ptr<GameFile>& elem) {
+    if (state.GetMode() == PointerWrap::MODE_READ)
+      elem = std::make_shared<GameFile>();
+    elem->DoState(state);
+  });
+}
+
+}  // namespace DiscIO

Source/Core/UICommon/GameFileCache.h
@@ -1,0 +1,58 @@
+// Copyright 2017 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+#include "Common/CommonTypes.h"
+
+class PointerWrap;
+
+namespace Core
+{
+class TitleDatabase;
+}
+
+namespace UICommon
+{
+class GameFile;
+
+std::vector<std::string> FindAllGamePaths(const std::vector<std::string>& directories_to_scan,
+                                          bool recursive_scan);
+
+class GameFileCache
+{
+public:
+  void ForEach(std::function<void(const std::shared_ptr<const GameFile>&)> f) const;
+
+  void Clear();
+
+  std::shared_ptr<const GameFile> AddOrGet(const std::string& path, bool* cache_changed,
+                                           const Core::TitleDatabase& title_database);
+
+  // These functions return true if the call modified the cache.
+  bool Update(const std::vector<std::string>& all_game_paths);
+  bool UpdateAdditionalMetadata(const Core::TitleDatabase& title_database);
+
+  bool Load();
+  bool Save();
+
+private:
+  bool UpdateAdditionalMetadata(std::shared_ptr<GameFile>* game_file,
+                                const Core::TitleDatabase& title_database);
+
+  bool SyncCacheFile(bool save);
+  void DoState(PointerWrap* p, u64 size = 0);
+
+  std::vector<std::shared_ptr<GameFile>> m_cached_files;
+};
+
+}  // namespace UICommon

Source/Core/DolphinQt2/GameList/GameFileCache.cpp
@@ -1,76 +1,0 @@
-// Copyright 2017 Dolphin Emulator Project
-// Licensed under GPLv2+
-// Refer to the license.txt file included.
-
-#include "GameFileCache.h"
-
-#include <QDataStream>
-#include <QDir>
-#include <QFile>
-#include <QFileInfo>
-
-#include "Common/FileUtil.h"
-#include "Core/ConfigManager.h"
-#include "DolphinQt2/Settings.h"
-
-static const int CACHE_VERSION = 4;  // Last changed in PR #6109
-static const int DATASTREAM_VERSION = QDataStream::Qt_5_0;
-
-GameFileCache::GameFileCache()
-    : m_path(QString::fromStdString(File::GetUserPath(D_CACHE_IDX) + "qt_gamefile.cache"))
-{
-}
-
-bool GameFileCache::IsCached(const QString& path) const
-{
-  return m_gamefiles.contains(path);
-}
-
-GameFile GameFileCache::GetFile(const QString& path) const
-{
-  return m_gamefiles[path];
-}
-
-void GameFileCache::Load()
-{
-  QFile file(m_path);
-
-  if (!file.open(QIODevice::ReadOnly))
-    return;
-
-  QDataStream stream(&file);
-  stream.setVersion(DATASTREAM_VERSION);
-
-  qint32 cache_version;
-  stream >> cache_version;
-
-  // If the cache file is using an older version, ignore it and create it from scratch
-  if (cache_version != CACHE_VERSION)
-    return;
-
-  stream >> m_gamefiles;
-}
-
-void GameFileCache::Save() const
-{
-  QFile file(m_path);
-
-  if (!file.open(QIODevice::WriteOnly))
-    return;
-
-  QDataStream stream(&file);
-  stream.setVersion(DATASTREAM_VERSION);
-
-  stream << static_cast<qint32>(CACHE_VERSION);
-  stream << m_gamefiles;
-}
-
-void GameFileCache::Update(const GameFile& gamefile)
-{
-  m_gamefiles[gamefile.GetFilePath()] = gamefile;
-}
-
-QList<QString> GameFileCache::GetCached() const
-{
-  return m_gamefiles.keys();
-}

Source/Core/DolphinQt2/GameList/GameFileCache.h
@@ -1,29 +1,0 @@
-// Copyright 2017 Dolphin Emulator Project
-// Licensed under GPLv2+
-// Refer to the license.txt file included.
-
-#pragma once
-
-#include <QList>
-#include <QMap>
-#include <QString>
-
-#include "DolphinQt2/GameList/GameFile.h"
-
-class GameFileCache
-{
-public:
-  explicit GameFileCache();
-
-  void Update(const GameFile& gamefile);
-  void Save() const;
-  void Load();
-  bool IsCached(const QString& path) const;
-  GameFile GetFile(const QString& path) const;
-  QList<QString> GetCached() const;
-
-private:
-  QString m_path;
-
-  QMap<QString, GameFile> m_gamefiles;
-};

commit 560429a66869ef46a29fce29d69e0de92f5a6b7d

DolphinQt2: Don't show invalid games in game list
Regression from 2e91464.

This problem doesn't happen in DolphinWX as far as I know, but if
you've ran into the problem in DolphinQt2, it will carry over to
DolphinWX because of the shared game list cache.

Source/Core/DolphinQt2/GameList/GameTracker.cpp
@@ -173,7 +173,9 @@ void GameTracker::LoadGame(const QString& path)
   if (!DiscIO::ShouldHideFromGameList(converted_path))
   {
     bool cache_changed = false;
-    emit GameLoaded(m_cache.AddOrGet(converted_path, &cache_changed, m_title_database));
+    auto game = m_cache.AddOrGet(converted_path, &cache_changed, m_title_database);
+    if (game)
+      emit GameLoaded(std::move(game));
     if (cache_changed)
       m_cache.Save();
   }

Source/Core/UICommon/GameFileCache.cpp
@@ -28,7 +28,7 @@
 
 namespace UICommon
 {
-static constexpr u32 CACHE_REVISION = 7;  // Last changed in PR 6281
+static constexpr u32 CACHE_REVISION = 8;  // Last changed in PR 6560
 
 std::vector<std::string> FindAllGamePaths(const std::vector<std::string>& directories_to_scan,
                                           bool recursive_scan)
@@ -60,7 +60,12 @@ std::shared_ptr<const GameFile> GameFileCache::AddOrGet(const std::string& path,
       [&path](const std::shared_ptr<GameFile>& file) { return file->GetFilePath() == path; });
   const bool found = it != m_cached_files.cend();
   if (!found)
-    m_cached_files.emplace_back(std::make_shared<GameFile>(path));
+  {
+    std::shared_ptr<UICommon::GameFile> game = std::make_shared<GameFile>(path);
+    if (!game->IsValid())
+      return nullptr;
+    m_cached_files.emplace_back(std::move(game));
+  }
   std::shared_ptr<GameFile>& result = found ? *it : m_cached_files.back();
   if (UpdateAdditionalMetadata(&result, title_database) || !found)
     *cache_changed = true;

Source/Core/UICommon/GameFileCache.h
@@ -35,6 +35,7 @@ public:
 
   void Clear();
 
+  // Returns nullptr if the file is invalid.
   std::shared_ptr<const GameFile> AddOrGet(const std::string& path, bool* cache_changed,
                                            const Core::TitleDatabase& title_database);
 