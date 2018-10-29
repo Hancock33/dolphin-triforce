dolphin-emu/dolphin

Android: Add game specific gc/wii controller settings

Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/settings/utils/SettingsFile.java
@@ -88,6 +88,7 @@ public final class SettingsFile
   public static final String KEY_WAIT_FOR_SHADERS = "WaitForShadersBeforeStarting";
 
   public static final String KEY_GCPAD_TYPE = "SIDevice";
+  public static final String KEY_GCPAD_G_TYPE = "PadType";
 
   public static final String KEY_GCBIND_A = "InputA_";
   public static final String KEY_GCBIND_B = "InputB_";
@@ -115,6 +116,10 @@ public final class SettingsFile
 
   public static final String KEY_WIIMOTE_TYPE = "Source";
   public static final String KEY_WIIMOTE_EXTENSION = "Extension";
+
+  // Controller keys for game specific settings
+  public static final String KEY_WIIMOTE_G_TYPE = "WiimoteSource";
+  public static final String KEY_WIIMOTE_PROFILE = "WiimoteProfile";
 
   public static final String KEY_WIIBIND_A = "WiimoteA_";
   public static final String KEY_WIIBIND_B = "WiimoteB_";
@@ -366,6 +371,12 @@ public final class SettingsFile
     return readFile(getGenericGameSettingsForAllRegions(gameId), true, view);
   }
 
+  public static HashMap<String, SettingSection> readWiimoteProfile(final String gameId,
+          final String padId)
+  {
+    String profile = gameId + "_Wii" + padId;
+    return readFile(getWiiProfile(profile, padId), true, null);
+  }
 
   /**
    * Saves a Settings HashMap to a given .ini file on disk. If unsuccessful, outputs an error
@@ -431,11 +442,41 @@ public final class SettingsFile
       for (String settingKey : sortedKeySet)
       {
         Setting setting = settings.get(settingKey);
-        NativeLibrary
-                .SetUserSetting(gameId, mapSectionNameFromIni(section.getName()), setting.getKey(),
-                        setting.getValueAsString());
+        // Special case. Extension gets saved into a controller profile
+        if (settingKey.contains(SettingsFile.KEY_WIIMOTE_EXTENSION))
+        {
+          saveCustomWiimoteSetting(gameId, setting);
+        }
+        else
+        {
+          NativeLibrary.SetUserSetting(gameId, mapSectionNameFromIni(section.getName()),
+                  setting.getKey(), setting.getValueAsString());
+        }
       }
     }
+  }
+
+  /**
+   * Saves the extension value in a profile and enables that profile. Extension is the only
+   * controller setting that is not saved in the main config.
+   *
+   * @param gameId
+   * @param setting
+   */
+  public static void saveCustomWiimoteSetting(final String gameId, final Setting setting)
+  {
+    if (setting.getSection().equals(Settings.SECTION_PROFILE))
+      return;
+    String padId =
+            setting.getKey().substring(setting.getKey().length() - 1, setting.getKey().length());
+    String profile = gameId + "_Wii" + padId;
+
+    NativeLibrary.SetProfileSetting(profile, Settings.SECTION_PROFILE, KEY_WIIMOTE_EXTENSION,
+            setting.getValueAsString());
+
+    // Enable the profile
+    NativeLibrary.SetUserSetting(gameId, Settings.SECTION_CONTROLS,
+            KEY_WIIMOTE_PROFILE + (Integer.valueOf(padId) + 1), profile);
   }
 
   private static String mapSectionNameFromIni(String generalSectionName)
@@ -483,8 +524,30 @@ public final class SettingsFile
 
   private static File getCustomGameSettingsFile(String gameId)
   {
+
     return new File(
             DirectoryInitializationService.getUserDirectory() + "/GameSettings/" + gameId + ".ini");
+  }
+
+  private static File getWiiProfile(String profile, String padId)
+  {
+    String wiiConfigPath =
+            DirectoryInitializationService.getUserDirectory() + "/Config/Profiles/Wiimote/" +
+                    profile + ".ini";
+
+    File wiiProfile = new File(wiiConfigPath);
+    // If it doesn't exist, create it
+    if (!wiiProfile.exists())
+    {
+      String defautlWiiProfilePath =
+              DirectoryInitializationService.getUserDirectory() +
+                      "/Config/Profiles/Wiimote/WiimoteProfile.ini";
+      DirectoryInitializationService.copyFile(defautlWiiProfilePath, wiiConfigPath);
+
+      NativeLibrary.SetProfileSetting(profile, Settings.SECTION_PROFILE, "Device",
+              "Android/" + (Integer.valueOf(padId) + 4) + "/Touchscreen");
+    }
+    return wiiProfile;
   }
 
   private static SettingSection sectionFromLine(String line, boolean isCustomGame)
@@ -621,5 +684,11 @@ public final class SettingsFile
   private static String settingAsString(Setting setting)
   {
     return setting.getKey() + " = " + setting.getValueAsString();
+  }
+
+  private static String customWiimoteExtSettingAsString(Setting setting)
+  {
+    return setting.getKey().substring(0, setting.getKey().length() - 1) + " = " +
+            setting.getValueAsString();
   }
 }

Hancock33/dolphin-triforce

Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/settings/utils/SettingsFile.java
@@ -90,6 +90,7 @@ public final class SettingsFile
   public static final String KEY_WAIT_FOR_SHADERS = "WaitForShadersBeforeStarting";
 
   public static final String KEY_GCPAD_TYPE = "SIDevice";
+  public static final String KEY_GCPAD_G_TYPE = "PadType";
 
   public static final String KEY_GCBIND_A = "InputA_";
   public static final String KEY_GCBIND_B = "InputB_";
@@ -119,6 +120,10 @@ public final class SettingsFile
 
   public static final String KEY_WIIMOTE_TYPE = "Source";
   public static final String KEY_WIIMOTE_EXTENSION = "Extension";
+
+  // Controller keys for game specific settings
+  public static final String KEY_WIIMOTE_G_TYPE = "WiimoteSource";
+  public static final String KEY_WIIMOTE_PROFILE = "WiimoteProfile";
 
   public static final String KEY_WIIBIND_A = "WiimoteA_";
   public static final String KEY_WIIBIND_B = "WiimoteB_";
@@ -370,6 +375,12 @@ public final class SettingsFile
     return readFile(getGenericGameSettingsForAllRegions(gameId), true, view);
   }
 
+  public static HashMap<String, SettingSection> readWiimoteProfile(final String gameId,
+          final String padId)
+  {
+    String profile = gameId + "_Wii" + padId;
+    return readFile(getWiiProfile(profile, padId), true, null);
+  }
 
   /**
    * Saves a Settings HashMap to a given .ini file on disk. If unsuccessful, outputs an error
@@ -435,11 +446,41 @@ public final class SettingsFile
       for (String settingKey : sortedKeySet)
       {
         Setting setting = settings.get(settingKey);
-        NativeLibrary
-                .SetUserSetting(gameId, mapSectionNameFromIni(section.getName()), setting.getKey(),
-                        setting.getValueAsString());
+        // Special case. Extension gets saved into a controller profile
+        if (settingKey.contains(SettingsFile.KEY_WIIMOTE_EXTENSION))
+        {
+          saveCustomWiimoteSetting(gameId, setting);
+        }
+        else
+        {
+          NativeLibrary.SetUserSetting(gameId, mapSectionNameFromIni(section.getName()),
+                  setting.getKey(), setting.getValueAsString());
+        }
       }
     }
+  }
+
+  /**
+   * Saves the extension value in a profile and enables that profile. Extension is the only
+   * controller setting that is not saved in the main config.
+   *
+   * @param gameId
+   * @param setting
+   */
+  public static void saveCustomWiimoteSetting(final String gameId, final Setting setting)
+  {
+    if (setting.getSection().equals(Settings.SECTION_PROFILE))
+      return;
+    String padId =
+            setting.getKey().substring(setting.getKey().length() - 1, setting.getKey().length());
+    String profile = gameId + "_Wii" + padId;
+
+    NativeLibrary.SetProfileSetting(profile, Settings.SECTION_PROFILE, KEY_WIIMOTE_EXTENSION,
+            setting.getValueAsString());
+
+    // Enable the profile
+    NativeLibrary.SetUserSetting(gameId, Settings.SECTION_CONTROLS,
+            KEY_WIIMOTE_PROFILE + (Integer.valueOf(padId) + 1), profile);
   }
 
   private static String mapSectionNameFromIni(String generalSectionName)
@@ -487,8 +528,30 @@ public final class SettingsFile
 
   private static File getCustomGameSettingsFile(String gameId)
   {
+
     return new File(
             DirectoryInitialization.getUserDirectory() + "/GameSettings/" + gameId + ".ini");
+  }
+
+  private static File getWiiProfile(String profile, String padId)
+  {
+    String wiiConfigPath =
+            DirectoryInitialization.getUserDirectory() + "/Config/Profiles/Wiimote/" +
+                    profile + ".ini";
+
+    File wiiProfile = new File(wiiConfigPath);
+    // If it doesn't exist, create it
+    if (!wiiProfile.exists())
+    {
+      String defautlWiiProfilePath =
+              DirectoryInitialization.getUserDirectory() +
+                      "/Config/Profiles/Wiimote/WiimoteProfile.ini";
+      DirectoryInitialization.copyFile(defautlWiiProfilePath, wiiConfigPath);
+
+      NativeLibrary.SetProfileSetting(profile, Settings.SECTION_PROFILE, "Device",
+              "Android/" + (Integer.valueOf(padId) + 4) + "/Touchscreen");
+    }
+    return wiiProfile;
   }
 
   private static SettingSection sectionFromLine(String line, boolean isCustomGame)
@@ -625,5 +688,11 @@ public final class SettingsFile
   private static String settingAsString(Setting setting)
   {
     return setting.getKey() + " = " + setting.getValueAsString();
+  }
+
+  private static String customWiimoteExtSettingAsString(Setting setting)
+  {
+    return setting.getKey().substring(0, setting.getKey().length() - 1) + " = " +
+            setting.getValueAsString();
   }
 }