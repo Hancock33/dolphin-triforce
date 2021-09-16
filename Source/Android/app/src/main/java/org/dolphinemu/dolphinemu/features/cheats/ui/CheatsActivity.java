JosJuice/dolphin

Android: Add checkboxes for toggling cheats enabled

Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
@@ -18,6 +18,10 @@ public class CheatsActivity extends AppCompatActivity
   private static final String ARG_GAME_ID = "game_id";
   private static final String ARG_REVISION = "revision";
 
+  private String mGameID;
+  private int mRevision;
+  private CheatsViewModel mViewModel;
+
   public static void launch(Context context, String gameId, int revision)
   {
     Intent intent = new Intent(context, CheatsActivity.class);
@@ -34,14 +38,22 @@ public class CheatsActivity extends AppCompatActivity
     MainPresenter.skipRescanningLibrary();
 
     Intent intent = getIntent();
-    String gameID = intent.getStringExtra(ARG_GAME_ID);
-    int revision = intent.getIntExtra(ARG_REVISION, 0);
+    mGameID = intent.getStringExtra(ARG_GAME_ID);
+    mRevision = intent.getIntExtra(ARG_REVISION, 0);
 
-    setTitle(getString(R.string.preferences_cheats_with_game_id, gameID));
+    setTitle(getString(R.string.preferences_cheats_with_game_id, mGameID));
 
-    CheatsViewModel viewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
-    viewModel.load(gameID, revision);
+    mViewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
+    mViewModel.load(mGameID, mRevision);
 
     setContentView(R.layout.activity_cheats);
+  }
+
+  @Override
+  protected void onStop()
+  {
+    super.onStop();
+
+    mViewModel.saveIfNeeded(mGameID, mRevision);
   }
 }

Source/Android/jni/Cheats/PatchCheat.cpp
@@ -39,6 +39,20 @@ Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_getName(JNIEnv* 
   return ToJString(env, GetPointer(env, obj)->name);
 }
 
+JNIEXPORT jboolean JNICALL
+Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_getEnabled(JNIEnv* env, jobject obj)
+{
+  return static_cast<jboolean>(GetPointer(env, obj)->enabled);
+}
+
+JNIEXPORT void JNICALL
+Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_setEnabledImpl(JNIEnv* env,
+                                                                               jobject obj,
+                                                                               jboolean enabled)
+{
+  GetPointer(env, obj)->enabled = static_cast<bool>(enabled);
+}
+
 JNIEXPORT jobjectArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_loadCodes(JNIEnv* env, jclass,
                                                                           jstring jGameID,
@@ -63,5 +77,28 @@ Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_loadCodes(JNIEnv
     env->SetObjectArrayElement(array, i++, PatchCheatToJava(env, patch));
 
   return array;
+}
+
+JNIEXPORT void JNICALL Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_saveCodes(
+    JNIEnv* env, jclass, jstring jGameID, jint revision, jobjectArray jCodes)
+{
+  const jsize size = env->GetArrayLength(jCodes);
+  std::vector<PatchEngine::Patch> vector;
+  vector.reserve(size);
+
+  for (jsize i = 0; i < size; ++i)
+  {
+    jobject code = reinterpret_cast<jstring>(env->GetObjectArrayElement(jCodes, i));
+    vector.emplace_back(*GetPointer(env, code));
+    env->DeleteLocalRef(code);
+  }
+
+  const std::string game_id = GetJString(env, jGameID);
+  const std::string ini_path = File::GetUserPath(D_GAMESETTINGS_IDX) + game_id + ".ini";
+
+  IniFile game_ini_local;
+  game_ini_local.Load(ini_path);
+  PatchEngine::SavePatches(&game_ini_local, vector);
+  game_ini_local.Save(ini_path);
 }
 }

Hancock33/dolphin-triforce

Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
@@ -18,6 +18,10 @@ public class CheatsActivity extends AppCompatActivity
   private static final String ARG_GAME_ID = "game_id";
   private static final String ARG_REVISION = "revision";
 
+  private String mGameId;
+  private int mRevision;
+  private CheatsViewModel mViewModel;
+
   public static void launch(Context context, String gameId, int revision)
   {
     Intent intent = new Intent(context, CheatsActivity.class);
@@ -34,14 +38,22 @@ public class CheatsActivity extends AppCompatActivity
     MainPresenter.skipRescanningLibrary();
 
     Intent intent = getIntent();
-    String gameId = intent.getStringExtra(ARG_GAME_ID);
-    int revision = intent.getIntExtra(ARG_REVISION, 0);
+    mGameId = intent.getStringExtra(ARG_GAME_ID);
+    mRevision = intent.getIntExtra(ARG_REVISION, 0);
 
-    setTitle(getString(R.string.cheats_with_game_id, gameId));
+    setTitle(getString(R.string.cheats_with_game_id, mGameId));
 
-    CheatsViewModel viewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
-    viewModel.load(gameId, revision);
+    mViewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
+    mViewModel.load(mGameId, mRevision);
 
     setContentView(R.layout.activity_cheats);
+  }
+
+  @Override
+  protected void onStop()
+  {
+    super.onStop();
+
+    mViewModel.saveIfNeeded(mGameId, mRevision);
   }
 }

Source/Android/jni/Cheats/PatchCheat.cpp
@@ -39,6 +39,20 @@ Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_getName(JNIEnv* 
   return ToJString(env, GetPointer(env, obj)->name);
 }
 
+JNIEXPORT jboolean JNICALL
+Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_getEnabled(JNIEnv* env, jobject obj)
+{
+  return static_cast<jboolean>(GetPointer(env, obj)->enabled);
+}
+
+JNIEXPORT void JNICALL
+Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_setEnabledImpl(JNIEnv* env,
+                                                                               jobject obj,
+                                                                               jboolean enabled)
+{
+  GetPointer(env, obj)->enabled = static_cast<bool>(enabled);
+}
+
 JNIEXPORT jobjectArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_loadCodes(JNIEnv* env, jclass,
                                                                           jstring jGameID,
@@ -63,5 +77,28 @@ Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_loadCodes(JNIEnv
     env->SetObjectArrayElement(array, i++, PatchCheatToJava(env, patch));
 
   return array;
+}
+
+JNIEXPORT void JNICALL Java_org_dolphinemu_dolphinemu_features_cheats_model_PatchCheat_saveCodes(
+    JNIEnv* env, jclass, jstring jGameID, jint revision, jobjectArray jCodes)
+{
+  const jsize size = env->GetArrayLength(jCodes);
+  std::vector<PatchEngine::Patch> vector;
+  vector.reserve(size);
+
+  for (jsize i = 0; i < size; ++i)
+  {
+    jobject code = reinterpret_cast<jstring>(env->GetObjectArrayElement(jCodes, i));
+    vector.emplace_back(*GetPointer(env, code));
+    env->DeleteLocalRef(code);
+  }
+
+  const std::string game_id = GetJString(env, jGameID);
+  const std::string ini_path = File::GetUserPath(D_GAMESETTINGS_IDX) + game_id + ".ini";
+
+  IniFile game_ini_local;
+  game_ini_local.Load(ini_path);
+  PatchEngine::SavePatchSection(&game_ini_local, vector);
+  game_ini_local.Save(ini_path);
 }
 }