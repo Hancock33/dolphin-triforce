Hancock33/dolphin-triforce

commit 0ac9e8edb26253e15657396d16f18e66cc87a9ec

JitArm64: Remove the ability to reuse backpatch slowmem handlers
Reusing the slowmem handlers of existing blocks meshes badly
with reusing the empty space left when destroying blocks.
I don't think reusing slowmem handlers was much of a gain anyway.

Source/Core/Core/PowerPC/JitArm64/Jit.cpp
@@ -123,7 +123,6 @@ bool JitArm64::HandleStackFault()
 void JitArm64::ClearCache()
 {
   m_fault_to_handler.clear();
-  m_handler_to_loc.clear();
 
   blocks.Clear();
   const Common::ScopedJITPageWriteAndNoExecute enable_jit_page_writes;

Source/Core/Core/PowerPC/JitArm64/Jit.h
@@ -178,21 +178,6 @@ public:
   bool IsFPRStoreSafe(size_t guest_reg) const;
 
 protected:
-  struct SlowmemHandler
-  {
-    Arm64Gen::ARM64Reg dest_reg;
-    Arm64Gen::ARM64Reg addr_reg;
-    BitSet32 gprs;
-    BitSet32 fprs;
-    u32 flags;
-
-    bool operator<(const SlowmemHandler& rhs) const
-    {
-      return std::tie(dest_reg, addr_reg, gprs, fprs, flags) <
-             std::tie(rhs.dest_reg, rhs.addr_reg, rhs.gprs, rhs.fprs, rhs.flags);
-    }
-  };
-
   struct FastmemArea
   {
     const u8* fastmem_code;
@@ -296,7 +281,6 @@ protected:
 
   // <Fastmem fault location, slowmem handler location>
   std::map<const u8*, FastmemArea> m_fault_to_handler;
-  std::map<SlowmemHandler, const u8*> m_handler_to_loc;
   Arm64GPRCache gpr;
   Arm64FPRCache fpr;
 

Source/Core/Core/PowerPC/JitArm64/JitArm64_BackPatch.cpp
@@ -122,39 +122,14 @@ void JitArm64::EmitBackpatchRoutine(u32 flags, bool fastmem, bool do_farcode, AR
   {
     if (fastmem && do_farcode)
     {
-      if (emitting_routine)
-      {
-        in_far_code = true;
-        SwitchToFarCode();
-      }
-      else
-      {
-        SlowmemHandler handler;
-        handler.dest_reg = RS;
-        handler.addr_reg = addr;
-        handler.gprs = gprs_to_push;
-        handler.fprs = fprs_to_push;
-        handler.flags = flags;
+      in_far_code = true;
+      SwitchToFarCode();
 
+      if (!emitting_routine)
+      {
         FastmemArea* fastmem_area = &m_fault_to_handler[fastmem_end];
-        auto handler_loc_iter = m_handler_to_loc.find(handler);
-
-        if (handler_loc_iter == m_handler_to_loc.end())
-        {
-          in_far_code = true;
-          SwitchToFarCode();
-          const u8* handler_loc = GetCodePtr();
-          m_handler_to_loc[handler] = handler_loc;
-          fastmem_area->fastmem_code = fastmem_start;
-          fastmem_area->slowmem_code = handler_loc;
-        }
-        else
-        {
-          const u8* handler_loc = handler_loc_iter->second;
-          fastmem_area->fastmem_code = fastmem_start;
-          fastmem_area->slowmem_code = handler_loc;
-          return;
-        }
+        fastmem_area->fastmem_code = fastmem_start;
+        fastmem_area->slowmem_code = GetCodePtr();
       }
     }
 

commit 713e44a066c8c1de7e78e2099746037c20789525

JitArm64: Allocate 64 MB for farcode
My change in 0ac9e8e caused farcode to fill up much more than before.
Most games still ran fine, but certain games would have multiple
cache clears per minute, on top of being overall slow.

Maybe there's something prettier we can do about this than just
allocating more RAM, but we have the resource budget for making
Dolphin use more RAM, so I consider increasing the size of the
cache to be a good solution at least for the time being.

At least for Prince of Persia: The Forgotten Sands, 48 MB isn't
enough to stop the cache clears, but 64 MB is. (I only played the
game for a few minutes when testing, though.)

Source/Core/Core/PowerPC/JitArm64/Jit.cpp
@@ -28,8 +28,12 @@
 using namespace Arm64Gen;
 
 constexpr size_t CODE_SIZE = 1024 * 1024 * 32;
-constexpr size_t FARCODE_SIZE = 1024 * 1024 * 16;
-constexpr size_t FARCODE_SIZE_MMU = 1024 * 1024 * 48;
+// We use a bigger farcode size for JitArm64 than Jit64, because JitArm64 always emits farcode
+// for the slow path of each loadstore instruction. Jit64 postpones emitting farcode until the
+// farcode actually is needed, saving it from having to emit farcode for most instructions.
+// TODO: Perhaps implement something similar to Jit64. But using more RAM isn't much of a problem.
+constexpr size_t FARCODE_SIZE = 1024 * 1024 * 64;
+constexpr size_t FARCODE_SIZE_MMU = 1024 * 1024 * 64;
 
 constexpr size_t STACK_SIZE = 2 * 1024 * 1024;
 constexpr size_t SAFE_STACK_SIZE = 512 * 1024;