a8e67ca9ff9603ef58900111cf5fd43b831ccd44
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-05-23 21:40:12 +0100

    Replace std::ostringstream usage with fmt::format

--- Source/Core/UICommon/UICommon.cpp
+++ Source/Core/UICommon/UICommon.cpp
@@ -14,6 +14,8 @@
 #include <shlobj.h>  // for SHGetFolderPath
 #endif
 
+#include <fmt/format.h>
+
 #include "Common/Common.h"
 #include "Common/CommonPaths.h"
 #include "Common/Config/Config.h"
@@ -496,10 +498,8 @@ std::string FormatSize(u64 bytes, int decimals)
 
   // Don't need exact values, only 5 most significant digits
   const double unit_size = std::pow(2, unit * 10);
-  std::ostringstream ss;
-  ss << std::fixed << std::setprecision(decimals);
-  ss << bytes / unit_size << ' ' << Common::GetStringT(unit_symbols[unit]);
-  return ss.str();
+  return fmt::format("{:.{}Lf} {}", bytes / unit_size, decimals,
+                     Common::GetStringT(unit_symbols[unit]));
 }
 
 }  // namespace UICommon
